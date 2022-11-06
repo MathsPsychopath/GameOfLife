9920ab015ee0f0ae20c59dd944e84a0ecfd69352
Aaron Chan <0aaronchan@gmail.com>
2022-11-06 19:34:48 +0000

    added GOL logic

--- gol/distributor.go
+++ gol/distributor.go
@@ -1,5 +1,12 @@
 package gol
 
+import (
+	// "fmt"
+	// "strconv"
+
+	"uk.ac.bris.cs/gameoflife/util"
+)
+
 type distributorChannels struct {
 	events     chan<- Event
 	ioCommand  chan<- ioCommand
@@ -9,18 +16,100 @@ type distributorChannels struct {
 	ioInput    <-chan uint8
 }
 
+// create a black 2D slice of size p.ImageHeight x p.ImageWidth
+func initialiseNewWorld(p Params) [][]byte {
+	world := make([][]byte, p.ImageHeight)
+	for i := range(world){
+		world[i] = make([]byte, p.ImageWidth)
+	}
+	return world
+}
+
+// count the number of neighbours that a particular cell has in the world
+func getNeighbourCount(world [][]byte, row, column int, p Params) int {
+	alive := 0
+	offsets := []util.Cell{
+		{X:-1,Y: -1},
+		{X:-1,Y: 0},
+		{X:-1,Y: 1},
+		{X:0, Y:-1},
+		{X:0, Y:1},
+		{X:1, Y:-1},
+		{X:1, Y:0},
+		{X:1, Y:1},
+	}
+	for _,offset := range offsets {
+		actualRow := (row + offset.X) % p.ImageHeight
+		if actualRow < 0 {
+			actualRow = p.ImageHeight - 1
+		}
+		actualCol := (column + offset.Y) % p.ImageWidth
+		if actualCol < 0 {
+			actualCol = p.ImageWidth - 1
+		}
+		if world[actualRow][actualCol] == 0xFF {
+			alive++
+		}
+	}
+	return alive
+}
+
+// complete 1 iteration of the world following Game of Life rules
+func evolve(world [][]byte, p Params) [][]byte {
+	newWorld := initialiseNewWorld(p);
+	for i, row := range world {
+		for j := range row {
+			neighbours := getNeighbourCount(world, i, j, p)
+			if neighbours < 2 || neighbours > 3{
+				newWorld[i][j] = 0x00
+			} else {
+				if world[i][j] == 0x00 && neighbours == 3{
+					newWorld[i][j] = 0xFF
+					continue
+				}
+				newWorld[i][j] = world[i][j]
+			}
+		}
+	}
+	return newWorld
+}
+
+// get a list of the alive cells existing in the world
+func getAliveCells(world [][]byte) []util.Cell {
+	var aliveCells []util.Cell
+
+	for i, row := range(world) {
+		for j, cell := range(row) {
+			if cell == 0xFF {
+				aliveCells = append(aliveCells, util.Cell{X:j, Y:i})
+			}
+		}
+	}
+	return aliveCells
+}
+
+
 // distributor divides the work between workers and interacts with other goroutines.
 func distributor(p Params, c distributorChannels) {
 
 	// TODO: Create a 2D slice to store the world.
+	world := initialiseNewWorld(p)
+
+	// TODO: Give the filename to the io.channels.filename channel
+	
+	// TODO: Populate blank 2D slice with world data from input
 
 	turn := 0
-
-
 	// TODO: Execute all turns of the Game of Life.
+	for ;turn < p.Turns; turn++  {
+		world = evolve(world, p);
+	}
+
+	// Get a slice of the alive cells
+	aliveCells := getAliveCells(world)
 
 	// TODO: Report the final state using FinalTurnCompleteEvent.
-
+	c.events <- FinalTurnComplete{CompletedTurns: p.Turns, Alive: aliveCells}
 
 	// Make sure that the Io has finished any output before exiting.
 	c.ioCommand <- ioCheckIdle
--- gol/gol.go
+++ gol/gol.go
@@ -15,13 +15,16 @@ func Run(p Params, events chan<- Event, keyPresses <-chan rune) {
 
 	ioCommand := make(chan ioCommand)
 	ioIdle := make(chan bool)
+	filename := make(chan string)
+	output := make(chan uint8)
+	input := make(chan uint8)
 
 	ioChannels := ioChannels{
 		command:  ioCommand,
 		idle:     ioIdle,
-		filename: nil,
-		output:   nil,
-		input:    nil,
+		filename: filename,
+		output:   output,
+		input:    input,
 	}
 	go startIo(p, ioChannels)
 
@@ -29,9 +32,9 @@ func Run(p Params, events chan<- Event, keyPresses <-chan rune) {
 		events:     events,
 		ioCommand:  ioCommand,
 		ioIdle:     ioIdle,
-		ioFilename: nil,
-		ioOutput:   nil,
-		ioInput:    nil,
+		ioFilename: filename,
+		ioOutput:   output,
+		ioInput:    input,
 	}
 	distributor(p, distributorChannels)
 }
