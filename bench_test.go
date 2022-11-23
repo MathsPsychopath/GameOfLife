5b324c792c9a1f0c041e5fcfdfb92d3f0c6e4d08
Ferdinand Hubbard <ferdinandhubbar@gmail.com>
2022-11-23 10:36:48 +0000

    automatic benchmark all configurations

--- bench_test.go
+++ bench_test.go
@@ -8,19 +8,30 @@ import (
 )
 
 func BenchmarkLocal(b *testing.B) {
-	p := gol.Params{
-		Turns:       1000,
-		Threads:     8,
-		ImageWidth:  512,
-		ImageHeight: 512,
+	turnConfs := []int{1}
+	threadConfs := []int{1, 2, 3, 4, 5} //, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16}
+	imageConfs := []int{16, 64, 128, 256, 512}
+	for _, threads := range threadConfs {
+		for _, imageSize := range imageConfs {
+			for _, turns := range turnConfs {
+				p := gol.Params{
+					Turns:       turns,
+					Threads:     threads,
+					ImageWidth:  imageSize,
+					ImageHeight: imageSize,
+				}
+				name := fmt.Sprintf("%dx%d_%d-threads_%d-turns", imageSize, imageSize, threads, turns)
+				b.Run(name, func(b *testing.B) {
+					benchmark(b, p)
+				})
+			}
+		}
 	}
-	benchmark(b, p)
+
 }
 
 func benchmark(b *testing.B, p gol.Params) {
-	fmt.Print("working\n")
-
-	for i := 0; i < 1; i++ {
+	for i := 0; i < b.N; i++ {
 		events := make(chan gol.Event)
 		go gol.Run(p, events, nil)
 
